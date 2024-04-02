08a883215a6f06aabcd158390d6b304981f343ca
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-04-02 08:55:59 -0500

    flash.vs -> flashapp.me

--- config/config.ts
+++ config/config.ts
@@ -2,4 +2,4 @@ export const USD_INVOICE_EXPIRE_INTERVAL = 60 * 5
 export const MAX_INPUT_VALUE_LENGTH = 14
 export const CASH_REGISTER_DESCRIPTION = "Flash POS Cash Register"
 export const APP_DESCRIPTION = "Flash official lightning network node"
-export const URL_HOST_DOMAIN = "flash.sv"
+export const URL_HOST_DOMAIN = "flashapp.me"
