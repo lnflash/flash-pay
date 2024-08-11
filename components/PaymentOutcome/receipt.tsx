9be26b1fe99596587fa777960dc4f70a0bd643a2
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-08-11 12:18:36 -0400

    format receipt for 58mm print

--- components/PaymentOutcome/receipt.tsx
+++ components/PaymentOutcome/receipt.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Image from "react-bootstrap/Image"
-import GaloyIcon from "./galoy-icon"
 import styles from "./payment-outcome.module.css"
 import { formattedDate, formattedTime } from "../../utils/dateUtil"
 
@@ -13,41 +12,56 @@ interface Props {
   paymentAmount: string | string[] | undefined
 }
 
-function receipt(props: Props) {
+function Receipt(props: Props) {
   return (
-    <div className="w-100">
+    <div className={`w-100 ${styles.printReceipt}`}>
       <div className="d-flex py-3 justify-content-center">
-        <Image src="/flash-qr-logo.png" width={50} height={50} />
+        <Image src="/flash-qr-logo.png" className={styles.printImage} />
       </div>
 
       <div className="text-center">
-        <span>Transaction Amount</span>
-        <h1>{localStorage.getItem("display")}</h1>
-        <h3>{localStorage.getItem("formattedSatsValue")}</h3>
-        <span> ~ {localStorage.getItem("formattedFiatValue")}</span>
-
+        <span className={styles.fontLarge}>Sale Completed</span>
+        {/* <h1 className={styles.fontXlarge}>{localStorage.getItem("display")}</h1> */}
+        <h3 className={styles.fontLarge}>
+          {"USD$"}
+          {
+            Array.isArray(props.paymentAmount)
+              ? (parseFloat(props.paymentAmount[0]) / 100).toFixed(2) // Handle array by using the first element
+              : props.paymentAmount
+              ? (parseFloat(props.paymentAmount) / 100).toFixed(2) // Handle string
+              : "0.00" // Fallback for undefined
+          }
+        </h3>
+        <span className={styles.fontXlarge}>
+          {" "}
+          ~ {localStorage.getItem("formattedFiatValue")}
+        </span>
         <div className="d-flex justify-content-center">
-          <table className="my-3 w-100">
-            <tr>
-              <td className="py-3 border-bottom">Beneficiary Name</td>
-              <td className="py-3 border-bottom">{props.username}</td>
-            </tr>
-            <tr>
-              <td className="py-3 border-bottom">Paid On</td>
-              <td className="py-3 border-bottom">
-                {formattedDate(new Date())} at <span>{formattedTime(new Date())}</span>
-              </td>
-            </tr>
-
-            <tr>
-              <td className="py-3 border-bottom">Status</td>
-              <td className="py-3 border-bottom">Paid</td>
-            </tr>
-
-            <tr>
-              <td className="py-3 border-bottom">Description</td>
-              <td className="py-3 border-bottom">{props.memo ? props.memo : "none"}</td>
-            </tr>
+          <table className={`my-3 w-100 ${styles.printTable}`}>
+            <tbody>
+              <tr>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>Paid To</td>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>
+                  {props.username}
+                </td>
+              </tr>
+              <tr>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>Paid On</td>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>
+                  {formattedDate(new Date())} at <span>{formattedTime(new Date())}</span>
+                </td>
+              </tr>
+              <tr>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>Status</td>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>Paid</td>
+              </tr>
+              <tr>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>Description</td>
+                <td className={`py-3 border-bottom ${styles.fontMedium}`}>
+                  {props.memo ? props.memo : "none"}
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
@@ -55,4 +69,4 @@ function receipt(props: Props) {
   )
 }
 
-export default receipt
+export default Receipt
